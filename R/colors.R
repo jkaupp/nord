97964db1d65fcfcfdabd46ccedee373b16c4c04a
jkaupp <jkaupp@gmail.com>
2017-12-18 17:20:51 -0500

    Added Go7 palettes

--- R/colors.R
+++ R/colors.R
@@ -10,9 +10,7 @@
 #' aurora
 #'
 #'
-#' There are also 9 Colour palettes extracted from the works of the [Group of Seven](https://en.wikipedia.org/wiki/Group_of_Seven_(artists)) and  one color palette drawn from [Lumina Borealis](https://www.luminaborealis.com/)
-#'
-#'@examples
+#' There are also 11 Colour palettes extracted from the works of the [Group of Seven](https://en.wikipedia.org/wiki/Group_of_Seven_(artists)) and one color palette drawn from [Lumina Borealis](https://www.luminaborealis.com/)
 
 nord_palettes <- list(#Nord Palette
                       polarnight = c("#2E3440", "#3B4252", "#434C5E", "#4C566A"),
@@ -22,4 +20,15 @@ nord_palettes <- list(#Nord Palette
                       #Lumina Palette
                       lumina = c("#EDDAEB", "#AD8CAE", "#4F93B8", "#306489", "#222B4C"),
                       #G7 Palettes
-                      mountain_forms = c("#184860", "#486078", "#d8d8d8", "#484860", "#181830"))
+                      mountain_forms = c("#184860", "#486078", "#d8d8d8", "#484860", "#181830"),
+                      silver_mine = c("#4B644B","#647D4B","#E1E1E1","#7D96AF","#647D96"),
+                      lake_superior = c("#7D4B19","#C89664","#C87d4B","#4B647D","#324B64","#19324B"),
+                      victory_bonds = c("#AF1900","#C83200","#E19600","#193264","#001964"),
+                      halifax_harbor = c("#E1C8AF","#C8AF96","#AF967D","#967D7D","#7D6464","#644B64","#4B324b"),
+                      moose_pond = c("#4B3232","#7D4B32","#966432","#AF7D32","#E19632","#E1AF4B","#C8C896","#4B4B4B"),
+                      algoma_forest = c("#4B4B4B","#967D4B", "#AFAF7D", "#C89632", "#647D64","#96AFAF","#7D96AF"),
+                      rocky_mountain = c("#BEBEBE", "#C8C8C8", "#DCD2C8","#D2C8C8", "#BEBEC8", "#B4B4BE"),
+                      red_mountain = c("#7D3232", "#7D4B4B", "#7D6464", "#AF967D", "#FAC87D", "#E1AF64","#C8964B","#32324B"),
+                      baie_mouton = c("#304890", "#7890A8", "#90A8C0", "#A8A8A8", "#C0C0A8", "#6A7E4F","#304848"),
+                      afternoon_prarie = c("#486090", "#6078A8", "#7890A8","#90A8C0","#F0D8C0","#D6BBCF", "#A8C0C0","#C0D8D8","#A8A890")
+                      )
--- R/testing.R
+++ R/testing.R
@@ -1,6 +1,16 @@
 library(ggplot2)
 library(viridis)
+library(purrr)
+library(scales)
 
-ggplot(diamonds, aes(carat, price)) +
-  geom_point(aes(color = price)) +
-  scale_color_nord(palette = "aurora", discrete = FALSE)
+# ggplot(diamonds, aes(carat, price)) +
+#   geom_point(aes(color = color)) +
+#   scale_color_nord(palette = "afternoon_prarie", discrete = TRUE)
+#
+#
+# #
+# # colors <- nord_palettes[["silver_mine"]] %>%
+# #   map_df(~as.data.frame(t(grDevices::col2rgb(col = .x)))) %>%
+# #   mutate(z = sqrt(red^2 + green^2 + blue^2))
+# #
+# # predict(prcomp(colors$z))
